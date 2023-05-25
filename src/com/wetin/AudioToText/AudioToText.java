5fd52a9f298be46f54d792e5fe178a4da2d74a57
wetinx <wetin206@qq.com>
2023-05-25 16:21:58 +0800

    第一次提交

--- src/com/wetin/AudioToText/AudioToText.java
+++ src/com/wetin/AudioToText/AudioToText.java
@@ -2,7 +2,10 @@ package com.wetin.AudioToText;
 
 /**
  * @version 1.0.1
- * @Author: xiao wetin
+ * @author:  we tin
  */
 public class AudioToText {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
